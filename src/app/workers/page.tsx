2f37202d78845266ac5ebb2d08dbef041538021a
zelalemzola <tzelalemtesfaye@gmail.com>
2024-11-25 21:56:43 +0300

    done

--- src/app/workers/page.tsx
+++ src/app/workers/page.tsx
@@ -179,9 +179,9 @@ const Workers = () => {
         </Dialog>
       </div>
 
-      {(!maidsData || !categoriesData) ? <div className='pt-[40%] text-xl text-primary font-bold mx-auto'>Loading Lawyer...</div>
+      {(!maidsData || !categoriesData) ? <div className='pt-[40%] text-xl text-primary font-bold mx-auto'>Loading Physician...</div>
         :
-        <div className="flex flex-wrap items-center justify-center gap-x-2 md:gap-x-8 gap-y-20 pt-[20%] md:pt-[15%] z-0 py-20 ">
+        <div className="flex flex-wrap items-center justify-center gap-x-2 md:gap-x-8 gap-y-20 py-20 pt-[27%] md:pt-[15%] z-0  h-screen">
           {filteredMaids.map((maid) => (
             <Card key={maid._id} className="relative shadow-lg py-4 md:p-4 scale-[90%] md:scale-100 bg-primary text-white hover:shadow-xl">
               <CardHeader>
