1d46ca4222cdbc1e478e078e8d665df68a219bbb
zelalemzola <tzelalemtesfaye@gmail.com>
2024-11-26 07:46:41 +0300

    done

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
@@ -11,6 +12,16 @@ const Footer = () => {
                 <p className='text-lg font-bold text-primary'>DoChat</p>
             </div>
             <p className="text-sm text-gray-400">Talk to a Doctor with our secure and friendly platfrom</p>
+            <div className="flex flex-col items-start gap-3">
+              <div className="flex items-center gap-3 flex-wrap">
+                <Link href='/about' className='text-sm text-primary font-light'>About</Link>
+                <Link href='/services' className='text-sm text-primary font-light'>services</Link>
+              </div>
+              <div className="flex items-center gap-3 flex-wrap">
+                <Link href='/chat' className='text-sm text-primary font-light'>Chat</Link>
+                <Link href='/' className='text-sm text-primary font-light'>Terms and conditions</Link>
+              </div>
+            </div>
         </div>
         <div className="flex flex-row flex-wrap gap-7 md:gap-20">
               <Instagram className='text-lg text-primary  p-1  font-bold scale-[200%]' />
